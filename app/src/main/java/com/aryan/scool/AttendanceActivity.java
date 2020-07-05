6b7db3b3f38bd3fc6b2c46babb59b46a082a062f
Aryan Rajbhandari <rajbhandari.aryanrb@gmail.com>
2020-07-05 09:03:51 +0545

    Post request for attendance

--- app/src/main/java/com/aryan/scool/AttendanceActivity.java
+++ app/src/main/java/com/aryan/scool/AttendanceActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.sql.Date;
@@ -18,7 +20,9 @@ import retrofit2.Response;
 public class AttendanceActivity extends AppCompatActivity implements TakeAttendance {
 
     RecyclerView rvAttendance;
+    Button btnAttendanceDone;
     List<UserModel> students;
+    int totalStudents = 0;
     List<AttendanceModel> attendanceList = new ArrayList<>();
 
     @Override
@@ -27,7 +31,15 @@ public class AttendanceActivity extends AppCompatActivity implements TakeAttenda
         setContentView(R.layout.activity_attendance);
 
         rvAttendance = findViewById(R.id.attendanceRV);
+        btnAttendanceDone = findViewById(R.id.btnAttendanceDone);
         getStudents();
+
+        btnAttendanceDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                completeAttendance();
+            }
+        });
 
     }
 
@@ -39,6 +51,7 @@ public class AttendanceActivity extends AppCompatActivity implements TakeAttenda
             @Override
             public void onResponse(Call<List<UserModel>> call, Response<List<UserModel>> response) {
                 students = response.body();
+                totalStudents = students.size();
                 AttendanceAdapter adapter = new AttendanceAdapter(students, AttendanceActivity.this);
                 rvAttendance.setAdapter(adapter);
                 rvAttendance.setLayoutManager(new LinearLayoutManager(AttendanceActivity.this));
@@ -60,6 +73,29 @@ public class AttendanceActivity extends AppCompatActivity implements TakeAttenda
         }
         else if(status.equals("Absent")){
             attendanceList.add(new AttendanceModel(id, "5eed9d7fe368a57f6471b45b", "5eed8730275fa4e4f73b8e78", Date.valueOf("2077-10-20"), false));
+        }
+    }
+
+    public void completeAttendance(){
+        if(attendanceList.size() != totalStudents){
+            return;
+        }
+        else {
+            for (int i = 0; i < attendanceList.size(); i++) {
+                AttendanceAPI attendanceAPI = RetrofitUrl.getInstance().create(AttendanceAPI.class);
+                Call<Void> attendanceCall = attendanceAPI.takeAttendance(attendanceList.get(i));
+                attendanceCall.enqueue(new Callback<Void>() {
+                    @Override
+                    public void onResponse(Call<Void> call, Response<Void> response) {
+                        Toast.makeText(AttendanceActivity.this, "Done Attendance", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Void> call, Throwable t) {
+
+                    }
+                });
+            }
         }
     }
 }
--- app/src/main/java/com/aryan/scool/TakeAttendance.java
+++ app/src/main/java/com/aryan/scool/TakeAttendance.java
@@ -1,6 +1,5 @@
 package com.aryan.scool;
 
 public interface TakeAttendance {
-
     void attendance(String id, String status);
 }
